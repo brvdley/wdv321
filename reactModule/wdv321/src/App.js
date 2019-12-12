9b324ab550c54eca1b67796c37bb200505edb65e
Brvdley <bradleyowens126@gmail.com>
2019-12-12 14:48:10 -0600

    To-Do-List update

--- reactModule/wdv321/src/App.js
+++ reactModule/wdv321/src/App.js
@@ -1,37 +1,65 @@
 import React from 'react';
-import friends from './friends.js';
 import './App.css';
 
 class App extends React.Component {
-  setCurrentFriend = (friend) => {
-    console.log(friend);
-    setState(
-      currentFriend: friend
-    )
+
+  setCurrentToDoItem = (toDoItem) => {
+    this.setState({
+      currentToDoItem: toDoItem
+    });
+  };
+
+  strikeThrough(index) {
+    const isItemSelected = this.state.selectedItems === index;
+    return isItemSelected ? "strike" : "" ;
   }
+
+  saveToDoListItem = (toDoItem) => {
+    this.setState({
+      toDoList: [
+        ...this.state.toDoList,
+        toDoItem
+      ]
+    });
+  };
+
   constructor(props) {
     super(props);
 
     this.state = {
-      currentFriend: null
+      currentToDoItem: null,
+      toDoList: [],
+      strikeThrough: false
     };
   }
+
   render() {
   return (
-    <div className="App">
-      {friends.map((friend) =>
-        <button
-          className="friend"
-          key={friend.lastName}
-          onClick={() => this.setCurrentFriend(friend)}
-        >
-          <p>{friend.firstName} {friend.lastName}</p>
-          <p>{friend.addressLine1}, {friend.city}, {friend.state} {friend.zipCode}</p>
-        </button>
-      )}
+    <div class="container">
+    <div class="input">
+      <h1> To Do List </h1>
+      <label>To Do Item: </label>
+      <input onChange={(event) => this.setCurrentToDoItem(event.target.value)}></input>
+      <button onClick={() => this.saveToDoListItem(this.state.currentToDoItem)}>
+        <p>Add Item</p>
+      </button>
+      <p>{this.state.currentToDoItem}</p>
+      </div>
+      <div class="list">
+        <div class="lgroup">
+        <h2>To Do Items</h2>
+        {
+          this.state.toDoList.map((item, index) => <p className={this.strikeThrough(index)} onClick={() => this.setState({ selectedItems: index})} key={index}>{item}</p>)
+        }
+        </div>
+      </div>
     </div>
   );
 };
-}
-
+};
 export default App;
+/*
+To Do List Program
+Input Fields
+Show The Items
+*/
